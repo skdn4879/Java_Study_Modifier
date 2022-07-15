2b19fe9a4e89f50eab323f0a5c41d650d2159202
skdn4879 <skanxkrl3645@naver.com>
2022-07-15 10:15:19 +0900

    Week2_Day5_2

--- src/kr/co/ex1/Ex1Main.java
+++ src/kr/co/ex1/Ex1Main.java
@@ -3,8 +3,36 @@ package kr.co.ex1;
 public class Ex1Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		/*Iu iu = new Iu();
+		iu.setName("IU");
+		iu.setAge(30);
+		
+		String name = iu.getName();
+		int age = iu.getAge();*/
+		
+		/*Iu test = Iu.createIu();
+		test.setName("IU");
+		test.setAge(30);
+		
+		System.out.println("생성된 이름 : " + test.getName() + ",  생성된 나이 : " + test.getAge());
+		
+		Iu test2 = Iu.createIu("wow", 10);
+		System.out.println("생성된 이름 : " + test2.getName() + ",  생성된 나이 : " + test2.getAge());
+		System.out.println();*/
+		
+		Iu test = Iu.createIu();
+		test.setName("IU");
+		test.setAge(30);
+		
+		System.out.println("생성된 이름 : " + test.getName() + ",  생성된 나이 : " + test.getAge());
+		
+		Iu test2 = Iu.createIu();
+		test2.setName("wow");
+		test2.setAge(20);
+		System.out.println("생성된 이름 : " + test2.getName() + ",  생성된 나이 : " + test2.getAge());
+		System.out.println("생성된 이름 : " + test.getName() + ",  생성된 나이 : " + test.getAge());
+		
 	}
 
 }
--- src/kr/co/ex1/Iu.java
+++ src/kr/co/ex1/Iu.java
@@ -3,6 +3,26 @@ package kr.co.ex1;
 public class Iu {
 	private String name;
 	private int age;
+	private static Iu iu;	//싱글톤패턴을 위해 멤버변수 static으로 자기자신
+
+	private Iu() {}	//생성자를 private로 지정
+	
+	public static Iu createIu() {
+		if(Iu.iu == null) {
+			Iu.iu = new Iu();
+		}
+		return Iu.iu;
+	}	//static은 어플리케이션에서 공통으로 사용가능, 클래스를 생성하지않고도 사용가능, 싱글톤패턴
+
+	private Iu(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	
+	public static Iu createIu(String name, int age) {
+		return new Iu(name, age);
+	}
 	
 	//setter
 	//public void set변수명(데이터타입 변수명){}
